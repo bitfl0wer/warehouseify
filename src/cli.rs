9b5c2c7e8d41c2eda0eec090c6d8e956dfcb1675
bitfl0wer <florian@pro-weber.com>
2025-04-21 20:19:00 +0200

    cli changes

--- src/cli.rs
+++ src/cli.rs
@@ -8,12 +8,13 @@ pub struct Args {
     #[arg(short, long, value_name = "FILE")]
     /// Path to a warehouseify config file. If not specified, will use default values.
     pub(crate) config: Option<PathBuf>,
-    /// Minisign secret key, used to sign the resulting binstall-ready crate.
+    #[arg(long, value_name = "MINISIGN_KEY")]
+    /// Minisign secret key, used to sign the resulting binstall-ready crate. Provide it here, or in the warehousify config file under options.signing_key.
     pub(crate) signing_key: Option<minisign::SecretKeyBox>,
     #[arg(short, long, action = clap::ArgAction::Count)]
     /// Turn on verbose logging. The default log level is "WARN".
-    /// Each instance of "v" in "-v" will increase the logging level by one. Available, additional
-    /// logging levels are INFO (-v), DEBUG (-vv) and TRACE (-vvv)
+    /// Each instance of "v" in "-v" will increase the logging level by one. Logging levels are
+    /// INFO (-v), DEBUG (-vv) and TRACE (-vvv)
     pub(crate) verbose: u8,
     #[arg(short, long, default_value_t = false)]
     /// Assume "yes" to all questions asked.
