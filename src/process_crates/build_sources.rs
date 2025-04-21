25b973012267f3e80ac389ac371514c821509ce9
bitfl0wer <florian@pro-weber.com>
2025-04-21 20:57:07 +0200

    add function stub

---
+++ src/process_crates/build_sources.rs
@@ -0,0 +1,10 @@
+use std::path::Path;
+
+use crate::StdErrorS;
+
+pub(crate) fn add_build_meta_info(
+    full_crate_path: &Path,
+    verifying_key: &str,
+) -> Result<(), StdErrorS> {
+    todo!()
+}
